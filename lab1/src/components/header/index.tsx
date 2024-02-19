194b9c61f4b79e05165cdebcf3ff851a4dd2f8e4
Plutokim <alexproduction48@gmail.com>
2024-02-19 21:13:12 +0200

    lab 1

---
+++ lab1/src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import { IonHeader, IonToolbar, IonTitle } from "@ionic/react"
+
+
+
+export const Header = () => (<IonHeader>
+    <IonToolbar style={{"padding": "10px"}}>
+      <IonTitle>
+        Лабораторна робота #1
+      </IonTitle>
+      <IonTitle>
+        Виконав студент групи КН-32 Козачек Олександр Олексійович
+      </IonTitle>
+      <IonTitle>
+        Варіант 15
+      </IonTitle>
+    </IonToolbar>
+    </IonHeader>)
+
+
+
